7331d991a27ca4b39c6e449a4401bd2d33bf11f2
tw-smith <92607103+tw-smith@users.noreply.github.com>
2022-07-27 22:32:26 +0100

    Fixes bug where canvas would be empty after crop
    The canvas appeared empty after cropping because the hRatio and vRatio variables
    were going to Infinity because image.width and image.height were at zero because
    they were being called before the image object had fully loaded. Fixed by moving
    the image.width and image.height calls inside the image.onload function.

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -10,52 +10,51 @@ function previewImg() {
 
 
   reader.readAsDataURL(file);
-  reader.onload = () => {
+  reader.onload = (e) => {
     image.src = reader.result;
-    hRatio = canvas.width / image.width;
-    vRatio = canvas.height / image.height;
-  //  ratio = Math.min(hRatio,vRatio);
-    ratio = 0.25; // FIXME Hard coding in 0.25 because hRatio and vRatio appear to go to Infinity for some reason. Variable scope maybe?
-
-    image.onload = () => { // This image.onload function is required to make the image preview appear. Without it the image preview only appears when stepping through the JS in debug mode.
-      ctx.drawImage(image,0,0,image.width,image.height,0,0,image.width*ratio,image.height*ratio);
-      canvas.removeAttribute("hidden");
-      offsetX = canvas.offsetLeft
-      offsetY = canvas.offsetTop
-    };
-    
-    canvas.onmousedown = (e) => {
-      canvas.style.cursor = "crosshair"
-      isDrawing = true;
-      startX = parseInt(e.pageX - offsetX);
-      startY = parseInt(e.pageY - offsetY);
-    }
-    
-    canvas.onmouseup = (e) => {
-      canvas.style.cursor = "default"
-      isDrawing = false;
-    }
-  
-    canvas.onmousemove = (e) => {
-      if (isDrawing) {
-        let mouseX = parseInt(e.pageX - offsetX);
-        let mouseY = parseInt(e.pageY - offsetY);
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        ctx.drawImage(image,0,0,image.width,image.height,0,0,image.width*ratio,image.height*ratio);
-        ctx.strokeStyle = "red";
-        ctx.beginPath();
-        endX = mouseX;
-        endY = mouseY;
-        deltaX = mouseX - startX;
-        deltaY = mouseY - startY;
-        ctx.rect(startX, startY, deltaX, deltaY);
-        ctx.stroke();
-        ctx.strokeText(mouseX,100,0) //TODO what are these here for
-        ctx.strokeText(mouseY,200,0)
-      }
-    }
   }
 
+  image.onload = () => {
+    hRatio = canvas.width / image.width;
+    vRatio = canvas.height / image.height;
+    ratio = Math.min(hRatio,vRatio);
+    ctx.drawImage(image,0,0,image.width,image.height,0,0,image.width*ratio,image.height*ratio);
+    canvas.removeAttribute("hidden");
+    offsetX = canvas.offsetLeft
+    offsetY = canvas.offsetTop
+  };
+    
+  canvas.onmousedown = (e) => {
+    canvas.style.cursor = "crosshair"
+    isDrawing = true;
+    startX = parseInt(e.pageX - offsetX);
+    startY = parseInt(e.pageY - offsetY);
+  }
+  
+  canvas.onmouseup = (e) => {
+    canvas.style.cursor = "default"
+    isDrawing = false;
+  }
+  
+  canvas.onmousemove = (e) => {
+    if (isDrawing) {
+      let mouseX = parseInt(e.pageX - offsetX);
+      let mouseY = parseInt(e.pageY - offsetY);
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+      ctx.drawImage(image,0,0,image.width,image.height,0,0,image.width*ratio,image.height*ratio);
+      ctx.strokeStyle = "red";
+      ctx.beginPath();
+      endX = mouseX;
+      endY = mouseY;
+      deltaX = mouseX - startX;
+      deltaY = mouseY - startY;
+      ctx.rect(startX, startY, deltaX, deltaY);
+      ctx.stroke();
+      ctx.strokeText(mouseX,100,0) //TODO what are these here for
+      ctx.strokeText(mouseY,200,0)
+    }
+  }
+  
 
   document.getElementById("crop-image-button").onclick = () => {
     console.log("crop function")
@@ -64,24 +63,10 @@ function previewImg() {
     var hCropRatio = canvas.width / cropWidth;
     var vCropRatio = canvas.height / cropHeight;
     cropRatio = Math.min(hCropRatio,vCropRatio);
-  //  console.log(hCropRatio)
-  //  console.log(vCropRatio)
-  //  console.log(cropRatio)
-    console.log("startX")
-    console.log(startX)
-    console.log("endX")
-    console.log(endX)
-    console.log(cropWidth)
-    console.log(cropHeight)
-    console.log(offsetX)
-
-    // FIXME draws a blank canvas
-   // console.log("cropper")
-    //console.log(image)
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.drawImage(image,startX/vRatio,startY/vRatio,cropWidth,cropHeight,0,0,cropWidth*hRatio,cropHeight*vRatio);
   }
-};
+}
 
 document.getElementById("submit-image-button").onclick = () => {
   imgDataURL = canvas.toDataURL();
@@ -95,5 +80,4 @@ document.getElementById("submit-image-button").onclick = () => {
   .then(response => response.json()) //FIXME returns TypeError: NetworkError when attempting to fetch resource
   .then(json => console.log(json))
   .catch(err => console.log('request failed', err));
-
 }
